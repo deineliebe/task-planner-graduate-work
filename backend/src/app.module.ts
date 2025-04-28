2ae608da13d2f78093a04aad12faa75e91122ae9
deineliebe <deine.kleine.liebe@gmail.com>
2025-04-28 06:25:56 +0300

    Some fixes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,7 +5,6 @@ import * as path from 'node:path';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { configProvider } from './app.config.provider';
 import { TaskModule } from './tasks/tasks.module';
-import { Tasks } from './tasks/entities/task.entity';
 
 @Module({
   imports: [
@@ -19,12 +18,12 @@ import { Tasks } from './tasks/entities/task.entity';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT),
-      username: process.env.POSTGRES_USER,
+      host: process.env.DATABASE_HOST || 'db',
+      port: parseInt(process.env.DATABASE_PORT) || 5432,
+      username: process.env.POSTGRES_USER || 'postgres',
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      entities: [Tasks],
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
     TaskModule,
