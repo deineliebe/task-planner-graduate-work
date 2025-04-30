fdce51e1e59c3884956213941462276b8fb18562
deine.kleine.liebe@gmail.com <ms.balashova.2001@gmail.com>
2025-04-30 21:08:17 +0300

    Fix usertasks entity

--- backend/src/usersTasks/entities/usersTasks.entity.ts
+++ backend/src/usersTasks/entities/usersTasks.entity.ts
@@ -1,12 +1,18 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, PrimaryColumn, ManyToOne } from 'typeorm';
 import { IsNumber } from 'class-validator';
+import { User } from '../../users/entities/user.entity';
+import { Tasks } from '../../tasks/entities/task.entity';
 
 @Entity()
 export class UserTasks {
+  @PrimaryColumn()
   @Column()
   @IsNumber()
+  @ManyToOne(() => Tasks, (task) => task.id)
   taskId: number;
+  @PrimaryColumn()
   @Column()
   @IsNumber()
+  @ManyToOne(() => User, (user) => user.id)
   userId: number;
 }
